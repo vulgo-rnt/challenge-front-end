5f8f36f0302c57650ac9c8477d9bbe9bdd0212da
Renan.T <rtoniolo4@gmail.com>
2023-10-08 12:15:15 -0300

    feat: pagination

--- src/components/MainList/index.jsx
+++ src/components/MainList/index.jsx
@@ -5,20 +5,22 @@ import styled from "styled-components"
 
 const ListStyled = styled.main`
     display: flex;
-    height: 1000px;
+    flex-wrap:wrap;
     overflow-x: hidden;
 `
 
 export default function MainList(){
     const [type,setType] = useState('micro')
     const [cardData , setCardData] = useState([])
+    const [page, setPage] = useState('1')
 
     useEffect(()=>{
-        fetch(`https://api.openbrewerydb.org/v1/breweries?by_type=${type}&per_page=60`)
+        fetch(`https://api.openbrewerydb.org/v1/breweries?by_type=${type}&page=${page}&per_page=20`)
         .then((data)=> data.json())
         .then((data)=> setCardData(data))
-    },[type])
+    },[type,page])
 
+ 
     return(
         <>
             <Filter setType={(param)=>setType(param)}/>
@@ -38,6 +40,14 @@ export default function MainList(){
                     })
                 }
             </ListStyled>
+            <div>
+                <button onClick={(event)=> 
+                    setPage(event.target.innerText)}>1</button>
+                <button onClick={(event)=> 
+                    setPage(event.target.innerText)}>2</button>
+                <button onClick={(event)=> 
+                    setPage(event.target.innerText)}>3</button>
+            </div>
         </>
     )
 }
