b2ebcacc1a013b0ab3bfb17bb521bef62f3a868e
Renan.T <rtoniolo4@gmail.com>
2023-10-07 22:08:24 -0300

    feat: fetch in api

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,4 +1,19 @@
 export default function Filter({setType}){
-    const selectOptions = []
-    return(<></>)
+    const selectOptions = [
+        'micro',
+        'nano',
+        'regional',
+        'brewpub',
+        'large',
+        'planning',
+        'bar',
+        'contract',
+        'proprietor',
+        'closed'
+    ]
+    return(<>
+        <select onChange={(event)=> setType(event.target.value)}>
+            {selectOptions.map((option)=>(<option key={option}>{option}</option>))}
+        </select>
+    </>)
 }
--- src/components/MainList/index.jsx
+++ src/components/MainList/index.jsx
@@ -1,31 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "../Card"
 import Filter from "../Filter"
 import styled from "styled-components"
 
 const ListStyled = styled.main`
     display: flex;
+    height: 1000px;
+    overflow-x: hidden;
 `
 
 export default function MainList(){
     const [type,setType] = useState('micro')
     const [cardData , setCardData] = useState([])
+
+    useEffect(()=>{
+        fetch(`https://api.openbrewerydb.org/v1/breweries?by_type=${type}&per_page=60`)
+        .then((data)=> data.json())
+        .then((data)=> setCardData(data))
+    },[type])
+
     return(
         <>
-            <Filter setType={setType}/>
+            <Filter setType={(param)=>setType(param)}/>
             <ListStyled>
                 {cardData.length > 0 && 
-                    cardData.map((card)=>{
+                    cardData.map((card,index)=>{
                         const info = {
-                                title:'',
-                                address: card["address_1"],
+                                title: card.name,
+                                address: card["address_1"] ,
                                 country: card.country,
                                 postalCode: card["postal_code"],
                                 state: card.state,
                                 breweryType: card["brewery_type"],
                                 city: card.city,
                             }
-                    return <Card info={info} />
+                    return <Card key={index} info={info} />
                     })
                 }
             </ListStyled>
